ca9aaffa7eb6e596484b258ce5d9a99ce72cdc50
Suhyun Park <me@shiftpsh.com>
2024-12-06 04:51:45 +0900

    fix: testing kakao login by allauth

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('kakao/login/callback/', views.kakao_callback),
+    # path('kakao/login/callback/', views.kakao_callback),
     path('', include('allauth.urls')),
     path('logout/', views.kakao_logout, name='logout'),
 ]
