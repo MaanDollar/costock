fb43afb0d4680a2db3c79690b85b545e76247595
Jinkyu OH <jinkyuoh@Jinkyuui-MacBookAir.local>
2024-12-06 00:55:55 +0900

    fix:kakao login

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('kakao/login/callback/', views.kakao_callback),
-    path('', include('allauth.urls')),
     path('logout/', views.kakao_logout, name='logout'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/hello/', include('hello.urls')),
+    path('accounts/', include('allauth.urls')),
     path('api/auth/', include('accounts.urls')),
     path('api/stock/', include('stock.urls')),
     path('api/ai/', include('ai.urls')),
