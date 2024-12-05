5e303f0e71d7fb1925ef592ed35fe93d9f405b57
Jinkyu OH <jinkyuoh@Jinkyuui-MacBookAir.local>
2024-12-06 01:22:22 +0900

    fix:kakao login

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path('api/hello/', include('hello.urls')),
     path('accounts/', include('allauth.urls')),
     path('api/auth/', include('accounts.urls')),
