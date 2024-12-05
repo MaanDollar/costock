3ae29f8cd6e66c06d9b2fc6b4d3daff262262a03
Suhyun Park <me@shiftpsh.com>
2024-12-06 05:16:34 +0900

    fix: cleanup

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('kakao/login/callback/', views.kakao_callback),
     path('', include('allauth.urls')),
     path('logout/', views.kakao_logout, name='logout'),
 ]
