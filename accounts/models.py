9bc8be070b418d7a07ff80fe1d0f2c7a858ca6b5
Jinkyu OH <jinkyuoh@Jinkyuui-MacBookAir.local>
2024-12-06 19:00:08 +0900

    fix:accounts added

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class User(models.Model):
-    nickname = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=200, unique=True)
     profile_image = models.URLField(max_length=500, blank=True, null=True)
