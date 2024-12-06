60091a440932912f7c774314ae0d50b729588f3e
Jinkyu OH <jinkyuoh@Jinkyuui-MacBookAir.local>
2024-12-06 14:40:54 +0900

    fix:session changed

--- stock/urls.py
+++ stock/urls.py
@@ -3,10 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('owned/<int:stock_id>/add/', views.add_owned, name='add_owned'),
-    path('owned/modify/', views.modify_owned, name='modify_owned'),
-    path('owned/delete/', views.delete_owned, name='delete_owned'),
-    path('recommended/<int:stock_id>/add/', views.add_recommended, name='add_recommended'),
-    path('recommended/modify/', views.modify_recommended, name='modify_recommended'),
-    path('recommended/delete/', views.delete_recommended, name='delete_recommended'),
+    path('owned/add/', views.add_owned, name='add_owned'),
+    path('owned/<int:stock_id>/modify/', views.modify_owned, name='modify_owned'),
+    path('owned/<int:stock_id>/delete/', views.delete_owned, name='delete_owned'),
+    path('recommended/add/', views.add_recommended, name='add_recommended'),
+    path('recommended/<int:stock_id>/delete/', views.delete_recommended, name='delete_recommended'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -36,22 +36,14 @@ def modify_owned(request, stock_id):
 
     return JsonResponse({'status': 'success', 'message': 'success'})
 
-def delete_owned(request):
+def delete_owned(request, stock_id):
+    stock = get_object_or_404(Owned, id=stock_id)
+
     if request.method == 'POST':
-
-        name = request.POST.get('name')
-        quantity = request.POST.get('quantity')
-        price = request.POST.get('price')
-
-        try:
-            stock = Owned.objects.get(name=name, quantity=quantity, price=price)
-            stock.delete()
-        except Owned.DoesNotExist:
-
-            error_message = "해당 종목을 찾을 수 없습니다."
-            return render(request, 'delete_stock.html', {'error': error_message})
+        stock.delete()
 
     return JsonResponse({'status': 'success', 'message': 'success'})
+
 
 def add_recommended(request):
     if request.method == 'POST':
@@ -65,43 +57,12 @@ def add_recommended(request):
             price=float(price)
         )
 
-    return JsonResponse({'status': 'success', 'message': 'success'})
+    return JsonResponse({'status': 'success', 'message': 'success'}
 
-
-def delete_recommended(request):
-    if request.method == 'POST':
-
-        name = request.POST.get('name')
-        quantity = request.POST.get('quantity')
-        price = request.POST.get('price')
-
-        try:
-            stock = Recommended.objects.get(name=name, quantity=quantity, price=price)
-            stock.delete()
-        except Recommended.DoesNotExist:
-
-            error_message = "해당 종목을 찾을 수 없습니다."
-            return render(request, 'delete_stock.html', {'error': error_message})
-
-    return JsonResponse({'status': 'success', 'message': 'success'})
-
-
-def modify_recommended(request, stock_id):
-
+def delete_recommended(request, stock_id):
     stock = get_object_or_404(Recommended, id=stock_id)
 
     if request.method == 'POST':
-
-        name = request.POST.get('name')
-        quantity = request.POST.get('quantity')
-        price = request.POST.get('price')
-
-        stock.name = name
-        stock.quantity = int(quantity)
-        stock.price = float(price)
-        stock.save()
-
-        return redirect('stock_list')
+        stock.delete()
 
     return JsonResponse({'status': 'success', 'message': 'success'})
-
