b92cbd43a56278b1594793d49ccffb81ecf6642f
Jinkyu OH <jinkyuoh@Jinkyuui-MacBookAir.local>
2024-12-06 15:53:02 +0900

    fix:session changed

--- ai/models.py
+++ ai/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Stock(models.Model):
-    name = models.CharField(max_length=100)  # 종목명
-    quantity = models.IntegerField()  # 수량
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # 매입 단가
+    StockCode = models.CharField(max_length=10, primary_key=True)
+    StockName = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
+class StockCorrelation(models.Model):
+    StockCode = models.CharField(max_length=10, primary_key=True)
+    RelatedStockCode = models.CharField(max_length=10)
+    Correlation = models.DecimalField(max_digits=5, decimal_places=4)
